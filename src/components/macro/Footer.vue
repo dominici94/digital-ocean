50621fc2f94d5ba71239b5324933cf8e34383011
dominici94 <mattiadominici94@gmail.com>
2022-02-03 14:03:42 +0100

    init structure

---
+++ src/components/macro/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+      questo è il mio footer
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
